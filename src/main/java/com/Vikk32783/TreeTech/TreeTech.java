675711f8e07f8ec190b3e2cf0e3eeba0109ef622
Mars32783 <vikk32783@gmail.com>
2016-12-02 02:04:14 -0800

    Basics

---
+++ src/main/java/com/Vikk32783/TreeTech/TreeTech.java
@@ -0,0 +1,31 @@
+package com.Vikk32783.TreeTech;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="TreeTech", name="Tree Tech", version="1.10.2-1.0")
+public class TreeTech
+{
+    @Mod.Instance("TreeTech")
+    public static TreeTech instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postinit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
